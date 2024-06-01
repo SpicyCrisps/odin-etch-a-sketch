e7b4a0193606eb0a6efb20d1cb79827a68d12b91
Spicy Crisps <spicycrisps@protonmail.com>
2024-06-01 10:35:04 -0400

    Unfinished grid resizing (CSS Flex issues). Cleaned up code. Beginnings of user color choice function.

--- scripts/main.js
+++ scripts/main.js
@@ -5,32 +5,56 @@ let sketchResolution = (16*16);
 let isCanvasReady = false;
 let userColor = "white";
 
-
-function canvasLoop()
+//Setup UI for this.
+function getUserColor()
 {
-    if(!isCanvasReady)
-        {
-            createCanvas();
-        }
-    else
-        {
-            paintCell();
-        }
-    
+    userColor = "red";
 }
+
+//Make this a nice UI, this is kind of poop.
+function gatherCanvasInfo()
+{
+    sketchResolution = prompt("Canvas Size?")
+    
+    if(sketchResolution > 64 || sketchResolution <= 0 || isNaN(sketchResolution))
+        {
+            prompt("Sorry! Try again")
+            gatherCanvasInfo()
+        }
+
+    sketchResolution = (sketchResolution*sketchResolution)
+}
+
 function createCanvas()
 {
     for(let i = 0; i < sketchResolution; i++)
         {
             let sketchCellTemp = document.createElement("div");
-            //sketchCellTemp.style.outline
             sketchCellTemp.id ="cell";
             sketchCellTemp.addEventListener("click", (event) => {sketchCellTemp.style.backgroundColor = userColor});
             sketchCellContainer.appendChild(sketchCellTemp);
-            
         }
         console.log("Canvas is ready!");
         isCanvasReady = true;
+}
+
+function resetCanvas()
+{
+    for(let i = 0; i < sketchResolution; i++)
+    {
+        //No error check if child is null. Should fix.
+        sketchCellContainer.removeChild(sketchCellTemp);
+    }
+    isCanvasReady = false;
+}
+
+function canvasLoop()
+{
+    if(!isCanvasReady)
+        {
+            gatherCanvasInfo();
+            createCanvas();
+        }
 }
 
 canvasLoop();
