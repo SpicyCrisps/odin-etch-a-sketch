943575fc5c923d08eb83d399bf75926351627891
Spicy Crisps <spicycrisps@protonmail.com>
2024-06-01 09:34:40 -0400

    Moved functions around, created a main function loop. Added event check for mouse click to paint, can paint chosen color (not finalized)

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,22 @@ const body = document.body;
 const sketchCellContainer = body.querySelector("#container")
 //const sketchCell = document.createElement("div");
 let sketchResolution = (16*16);
+let isCanvasReady = false;
+let userColor = "white";
 
+
+function canvasLoop()
+{
+    if(!isCanvasReady)
+        {
+            createCanvas();
+        }
+    else
+        {
+            paintCell();
+        }
+    
+}
 function createCanvas()
 {
     for(let i = 0; i < sketchResolution; i++)
@@ -10,9 +25,12 @@ function createCanvas()
             let sketchCellTemp = document.createElement("div");
             //sketchCellTemp.style.outline
             sketchCellTemp.id ="cell";
+            sketchCellTemp.addEventListener("click", (event) => {sketchCellTemp.style.backgroundColor = userColor});
             sketchCellContainer.appendChild(sketchCellTemp);
             
         }
+        console.log("Canvas is ready!");
+        isCanvasReady = true;
 }
 
-createCanvas();
+canvasLoop();
